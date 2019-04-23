44036e35f19fbaa27d6b87792624500b0c927914
Rafael Jeferson <rafa.jefer@gmail.com>
2019-04-23 16:37:32 -0300

    Migrando meus-anuncios e editar-anuncios para blade

---
+++ resources/views/meus-anuncios.blade.php
@@ -0,0 +1,48 @@
+@extends('template')
+
+@section('conteudo')
+  <h1>Meus Anúncios</h1>   
+  <div class="clearfix mb-3">
+    <div class="float-right">
+      <a href="#" class="btn btn-secondary">Adicionar Anúncio</a>
+    </div>
+  </div>
+  @if(empty($anuncios))
+  <div class="alert alert-dark" role="alert">
+    Você não tem nenhum anúncio cadastrado.
+  </div>
+  @else
+  <div class="table-responsive">
+    <table class="table table-striped table-bordered table-borderless table-hover">
+      <caption>Total de anúncios do usuário: {{ count($anuncios) }}</caption>
+        <thead class="thead-dark">
+          <tr>
+            <th scope="col">Foto</th>
+            <th scope="col">Título</th>
+            <th scope="col">Valor</th>
+            <th scope="col">Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+         	@foreach ($anuncios as $a)
+          <tr>
+            <th class="align-middle">
+              @if(!empty($a->url))
+             	  <img src="/images/anuncios/9d005c0336e43ec641bf16f0ffa72a35.jpg" height="50" />
+              @else
+                <img src="/images/anuncios/default.jpg" height="50" />
+              @endif
+            </th>
+            <td class="align-middle">{{ $a->titulo }}</td>
+            <td class="align-middle">R$ {{ number_format($a->valor, 2, ",", ".") }}</td>
+            <td class="align-middle">
+              <a href="/anuncios/editar/{{$a->id}}" class="btn btn-sm btn-info">Editar</a>
+              <a href="/anuncios/excluir/{{$a->id}}" class="btn btn-sm btn-danger">Excluir</a>
+            </td>
+     	    </tr>
+       		@endforeach
+    	  </tbody>
+   	</table>
+    @endif
+  </div>   
+  @stop
