ef5f89b783aadef22cfad40f1369e33eba7e99b1
Rafael Jeferson <rafa.jefer@gmail.com>
2019-04-23 20:45:25 -0300

    Adicionado badge novo na listagem de meus anúncios

--- resources/views/meus-anuncios.blade.php
+++ resources/views/meus-anuncios.blade.php
@@ -17,10 +17,10 @@
       <caption>Total de anúncios do usuário: {{ count($anuncios) }}</caption>
         <thead class="thead-dark">
           <tr>
-            <th scope="col">Foto</th>
+            <th scope="col" width="90">Foto</th>
             <th scope="col">Título</th>
-            <th scope="col">Valor</th>
-            <th scope="col">Ações</th>
+            <th scope="col" width="150">Valor</th>
+            <th scope="col" width="150">Ações</th>
           </tr>
         </thead>
         <tbody>
@@ -33,7 +33,7 @@
                 <img src="/images/anuncios/default.jpg" height="50" />
               @endif
             </th>
-            <td class="align-middle">{{ $a->titulo }}</td>
+            <td class="align-middle">{{ $a->titulo }} <?= date('Y-m-d', strtotime($a->created_at)) > date('Y-m-d', strtotime('-1 week')) ? '<sup><span class="badge badge-success">Novo</span></sup>':'' ?></td>
             <td class="align-middle">R$ {{ number_format($a->valor, 2, ",", ".") }}</td>
             <td class="align-middle">
               <a href="/anuncios/editar/{{$a->id}}" class="btn btn-sm btn-info">Editar</a>
